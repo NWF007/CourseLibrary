caa56fc9673c1da2c6a7ab51c62e5ad74158ed09
Nico <ncfortuin27@gmail.com>
2021-02-04 16:39:50 +0200

    first commit

---
+++ CourseLibrary/Controllers/AuthorsController.cs
@@ -0,0 +1,41 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository courseLibraryRepository;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            this.courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+            var authorsFromRepo = courseLibraryRepository.GetAuthors();
+            return Ok(authorsFromRepo);
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var authorFromRepo = courseLibraryRepository.GetAuthor(authorId);
+         
+            if(authorFromRepo == null)
+            {
+                return NotFound();
+            }
+            return Ok(authorFromRepo);
+        }
+    }
+}
